6dc6948df866f3e5087c0e7f06fcff36a1ce236d
crisavendano <coavendano@uc.cl>
2022-12-24 21:49:23 -0300

    chore: contact info

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,12 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.flex}>
-        <p className={styles.content}>Cristóbal Avendaño </p>
-        <p className={styles.content}><a href="https://github.com/crisavendano"> GitHub</a></p>
+        <p className={styles.content}><a href="https://github.com/crisavendano/mean">GitHub</a></p>
+        <p>
+          <a href="mailto:coavendano@uc.cl?Subject=Statistics%20Calculator%20recommendation%20or%20calculation%20error">
+            Recommendations or calculation errors (always welcome!)
+          </a>
+        </p>
       </div>
     </div>
   );
